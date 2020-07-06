74e0197d1f4735d4100e0115d2105bf03c4208f2
bdemirkazik <67442942+bdemirkazik@users.noreply.github.com>
2020-07-05 20:13:54 -0500

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den Merhaba");
-        ystem.out.println("GitHub'dan Merhaba");
+        System.out.println("GitHub'dan Merhaba");
     }
 }
